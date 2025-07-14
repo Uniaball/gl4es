af6d7a1b8211361b3ae96c952036d4e850850c25
Uniaball <2551733616@qq.com>
2025-07-14 17:11:42 +0800

    Update string_utils.h

--- src/gl/string_utils.h
+++ src/gl/string_utils.h
@@ -3,15 +3,15 @@
 
 extern const char* AllSeparators;
 
-const char* gl4es_find_string(const char* pBuffer, const char* S);
+const char* FindString(const char* pBuffer, const char* S);
 char* gl4es_find_string_nc(char* pBuffer, const char* S);
 int gl4es_count_string(const char* pBuffer, const char* S);
 char* gl4es_resize_if_needed(char* pBuffer, int *size, int addsize);
-char* gl4es_inplace_replace(char* pBuffer, int* size, const char* S, const char* D);
+char* InplaceReplace(char* pBuffer, int* size, const char* S, const char* D);
 char* gl4es_append(char* pBuffer, int* size, const char* S);
-char* gl4es_inplace_insert(char* pBuffer, const char* S, char* master, int* size);
-char* gl4es_getline(char* pBuffer, int num);
-int gl4es_countline(const char* pBuffer);
+char* InplaceInsert(char* pBuffer, const char* S, char* master, int* size);
+char* Getline(char* pBuffer, int num);
+int Countline(const char* pBuffer);
 int gl4es_getline_for(const char* pBuffer, const char* S); // get the line number for 1st occurent of S in pBuffer
 char* gl4es_str_next(char *pBuffer, const char* S); // mostly as strstr, but go after the substring if found
 //"blank" (space, tab, cr, lf,":", ",", ";", ".", "/")
@@ -20,7 +20,7 @@ char* gl4es_prev_str(char* Str, char* pBuffer);    // go to previous non "blank"
 char* gl4es_next_blank(char* pBuffer);   // go to next "blank"
 char* gl4es_next_line(char* pBuffer);   // go to next new line (crlf not included)
 
-const char* gl4es_get_next_str(char* pBuffer); // get a (static) copy of next str (until next separator), can be a simple number or separator also
+const char* GetNextStr(char* pBuffer); // get a (static) copy of next str (until next separator), can be a simple number or separator also
 
 // those function don't try to be smart with separators...
 int gl4es_countstring_simple(char* pBuffer, const char* S);
