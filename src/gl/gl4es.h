ff4a3b57c836edf0ddab2bd0087f1cbc7fcce24f
Uniaball <2551733616@qq.com>
2025-08-22 13:13:15 +0800

    [Fix] (gl4es.h): fix declaration problem

--- src/gl/gl4es.h
+++ src/gl/gl4es.h
@@ -86,7 +86,10 @@ void APIENTRY_GL4ES gl4es_glStencilMaskSeparate(GLenum face, GLuint mask);
 
 void APIENTRY_GL4ES gl4es_glMultiDrawArrays(GLenum mode, const GLint *first, const GLsizei *count, GLsizei primcount);
 void APIENTRY_GL4ES gl4es_glMultiDrawElements( GLenum mode, GLsizei *count, GLenum type, const void * const *indices, GLsizei primcount);
-void APIENTRY_GL4ES gl4es_glMultiDrawElementsBaseVertex( GLenum mode, GLsizei *count, GLenum type, const void * const *indices, GLsizei primcount, const GLint * basevertex);
+
+void APIENTRY_GL4ES gl4es_glMultiDrawElementsBaseVertex(GLenum mode, const GLsizei* counts, GLenum type,
+                                                            const void* const* indices, GLsizei primcount,
+                                                            const GLint* basevertex);
 
 void APIENTRY_GL4ES gl4es_glDrawElementsBaseVertex(GLenum mode, GLsizei count, GLenum type, const void *indices, GLint basevertex);
 void APIENTRY_GL4ES gl4es_glDrawRangeElementsBaseVertex(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type, const void *indices, GLint basevertex);
